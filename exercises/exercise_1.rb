8ff51896e5f3d8a1bb458f1cc58e0dd8ac902e20
antosha-85 <antosha-85@yandex.ru>
2020-03-03 02:08:04 +0000

    Finished exercise 5

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,7 +9,7 @@ end
 
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
-richmond = Store.create(name: "Richmond", annual_revenue: 12600000, mens_apparel: false, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
 gastown = Store.create(name: "Gastown", annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
 
