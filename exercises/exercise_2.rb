f312e97851327d73c6c05e6dae8570955a2d234c
Anton Smirnov <antosha-85@yandex.ru>
2020-03-02 18:14:45 -0700

    finished 2 exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+
+class Store < ActiveRecord::Base
+end
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Burnaby1")
+
+puts @store1.to_json
+puts @store2.to_json
